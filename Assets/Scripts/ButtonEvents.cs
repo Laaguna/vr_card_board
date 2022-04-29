338f7a829f8e1757a9db660538658b5b7de5f20a
Uckerze <73413231+Uckerze@users.noreply.github.com>
2022-04-28 22:04:35 -0500

    A
    Tengo sueño

---
+++ Assets/Scripts/ButtonEvents.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEvents : MonoBehaviour
+{
+    public Transform playerRoot, irAJugar_Position;
+    public Animator animatorPlayer;
+    public GameObject panelPrincipal;
+
+   public void IrAJugar(){
+       playerRoot.transform.position = irAJugar_Position.transform.position;
+       panelPrincipal.SetActive(false);
+   }
+
+   public void IrAPartes(){
+
+   }
+}

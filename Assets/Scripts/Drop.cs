c2e5dfddfd84580505dece90c79779d5304f9fe2
Uckerze <73413231+Uckerze@users.noreply.github.com>
2022-05-03 19:21:31 -0500

    Ojalà se los culeen a todos

---
+++ Assets/Scripts/Drop.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop : MonoBehaviour
+{
+        /// <summary>
+    /// The material to use when this object is inactive (not being gazed at).
+    /// </summary>
+    public Material InactiveMaterial;
+
+    /// <summary>
+    /// The material to use when this object is active (gazed at).
+    /// </summary>
+    public Material GazedAtMaterial;
+
+    // The objects are about 1 meter in radius, so the min/max target distance are
+    // set so that the objects are always within the room (which is about 5 meters
+    // across).
+    private const float _minObjectDistance = 2.5f;
+    private const float _maxObjectDistance = 3.5f;
+    private const float _minObjectHeight = 0.5f;
+    private const float _maxObjectHeight = 3.5f;
+
+    private Renderer _myRenderer;
+    private Vector3 _startingPosition;
+
+    public GameObject playerHand;
+    public Transform playerHandChild;
+
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
+    public void Start()
+    {
+        playerHandChild = ParentGameObjectt.transform.GetChild(0).gameObject;
+    }
+
+    
+
+    /// <summary>
+    /// This method is called by the Main Camera when it starts gazing at this GameObject.
+    /// </summary>
+    public void OnPointerEnter()
+    {
+        SetMaterial(true);
+        this.gameObject.transform.position = playerHand.transform.position;
+        this.gameObject.transform.SetParent(playerHand.transform);
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it stops gazing at this GameObject.
+    /// </summary>
+    public void OnPointerExit()
+    {
+        SetMaterial(false);
+    }
+
+    /// <summary>
+    /// This method is called by the Main Camera when it is gazing at this GameObject and the screen
+    /// is touched.
+    /// </summary>
+    public void OnPointerClick()
+    {
+        //TeleportRandomly();
+        this.gameObject.transform.position = playerHand.transform.position;
+    }
+
+    /// <summary>
+    /// Sets this instance's material according to gazedAt status.
+    /// </summary>
+    ///
+    /// <param name="gazedAt">
+    /// Value `true` if this object is being gazed at, `false` otherwise.
+    /// </param>
+    private void SetMaterial(bool gazedAt)
+    {
+        if (InactiveMaterial != null && GazedAtMaterial != null)
+        {
+            _myRenderer.material = gazedAt ? GazedAtMaterial : InactiveMaterial;
+        }
+    }
+}
